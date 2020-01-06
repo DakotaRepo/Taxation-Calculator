fa353fd59a67e5bf9adcb50cd956909a8bb56759
Dakota Teall <code9654@outlook.com>
2020-01-05 20:16:15 -0500

    Initial setup

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+int main(){
+
+    std::cout << "Hello World";
+    return 0;
+
+}
