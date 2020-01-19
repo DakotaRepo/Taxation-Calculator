2b145de1fa5ee182b112edefcbe5a80f684694be
Dakota Teall <code9654@outlook.com>
2020-01-19 00:47:02 -0500

    Progressive taxation functional

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ void PhaseThree();
 void PhaseFinal();
 int userIncome;
 int userTaxes = 0;
-int userTaxExemption = 0;
+int userTaxExcemption = 0;
+int sumTotal;
 
 int main(){
     Taxation();
@@ -22,30 +23,40 @@ int main(){
 }
 
 void PhaseOne(){
-    userTaxExemption = 10000;
-    userIncome = userIncome - 10000;
+    if(userIncome > 30000){
+        userTaxes = 2000;
+    }else {
+    userTaxExcemption = 10000;
+    userIncome -= 10000;
     userTaxes = userIncome * .10;
-    userIncome = userIncome * .90;
+    userIncome *= .90;
+    }
 }
 void PhaseTwo(){
-    userTaxExemption = userTaxExemption + 20000;
-    userIncome = userIncome - 20000;
-    userTaxes = userTaxes + (userIncome * .25);
-    userIncome = userIncome * .75;
+    if (userIncome > 100000){
+        userTaxes = 19500;
+    }else{
+    userTaxExcemption = 28000;
+    userIncome -= 30000;
+    userTaxes += userIncome * .25;
+    userIncome *= .75;
+    }
 }
 void PhaseThree(){
-    userTaxExemption = userTaxExemption + 70000;
-    userIncome = userIncome - 70000;
-    userTaxes = userTaxes + (userIncome * .40);
-    userIncome = userIncome * .60;
+    userTaxExcemption = 80500;
+    userIncome -= 100000;
+    userTaxes += userIncome * .40;
+    userIncome *= .60;
 }
 void PhaseFinal(){
-    userIncome = userIncome + userTaxExemption;
+    userIncome += userTaxExcemption;
+    if(userIncome + userTaxes < sumTotal) userIncome ++;
     std::cout << "Your total taxation is: " << userTaxes << "\nYour income after taxes is: " << userIncome;
 }
 void Taxation(){
     std::cout << "Enter your income\n";
-    std::cin >> userIncome;
+    std::cin >> sumTotal;
+    userIncome = sumTotal;
      if(userIncome > 10000 && userIncome <= 30000){
         PhaseOne();
         PhaseFinal();
